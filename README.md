606794c652e687463c5ce56f05dea1ffa4282365
your_username <antoine.alejandro@gmail.com>
2022-01-21 16:55:23 +0800

    push new changes

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
 # Learning Web - 1 - HTML:CSS
-
-## Pre Requisites
-
-### Visual Studio Code
-Got to [code.visualstudio.com](https://code.visualstudio.com/download) and download the latest version for your OS.
-Once VSCode is installed you can launch it.
-
-### Run the install script
-Open your terminal and paste the following code:
+## Run the install script
+Open your terminal and paste the following code with the right token:
 ```
-source <(curl -s https://raw.githubusercontent.com/antoinealej/test-dasdf/main/scripts/install.command)
+source < (curl -s https://raw.githubusercontent.com/antoinealej/test-dasdf/main/scripts/install.command) [TOKEN]
 ```
 
 ## The content of the L&L will be available [there](docs/LNLContent.md) after the session...
--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,10 @@ BREWPATH=$(which brew);
 [ ! $BREWPATH ] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 
+# Install VSCode if it's not already installed
+CODEPATH=$(which code);
+[ ! $CODEPATH ] && [ ! -d "/Applications/Visual Studio Code.app" ] && (brew install --cask visual-studio-code);
+
 # Install Git if it's not already installed
 GITPATH=$(which git);
 [ ! $GITPATH ] && brew install git;
@@ -22,10 +26,11 @@ BASICFOLDER=$LEARNWEBFOLDER/1-basic-html-css;
 [ ! -d $LEARNWEBFOLDER ] && mkdir $LEARNWEBFOLDER
 
 # Clone the git repository
-ACCESS_TOKEN=$1
-git clone https://$ACCESS_TOKEN@github.com/antoinealej/test-dasdf.git $BASICFOLDER
+ACCESS_TOKEN=$1;
+[ ! -d ${BASICFOLDER} ] && git clone https://$ACCESS_TOKEN@github.com/antoinealej/test-dasdf.git $BASICFOLDER
 cd $BASICFOLDER;
 git config user.name "antoinealejautomate";
+git pull;
 
 # Open VSCode with the project
-/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code ~/lunch-and-learn/learning-web/1-basic-html-css
+code ~/lunch-and-learn/learning-web/1-basic-html-css
